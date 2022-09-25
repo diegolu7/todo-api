e8f3f5e7d4b7b1e2062f3b52488bbaeb0e49f616
diegolu7 <diegolu7@gmail.com>
2022-09-25 00:41:20 -0300

    router + estilos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
+import PokeApi from './components/PokeApi';
+import Home from './components/home';
+import Navegacion from './layouts/navbar';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<BrowserRouter>
+<Routes>
+  <Route path='/' element={ <Navegacion /> }>
+    <Route index element={ <Home /> } />
+    <Route path='TodoList' element={ <TodoList /> } />
+    <Route path='PokeApi' element={ <PokeApi /> } />
+    <Route path='*' element={ <Navigate replace to="/"/> }/>
+  </Route>
+</Routes> 
+</BrowserRouter>
+
     </div>
   );
 }
---
+++ src/components/PokeApi.js
@@ -0,0 +1,8 @@
+const PokeApi = () => {
+    return(
+        <div>
+            <h1>PokeApi</h1>
+        </div>
+    )
+}
+export default PokeApi
