83f8956038df84c83aefd2606fcf8db175180d05
diegolu7 <diegolu7@gmail.com>
2022-09-27 21:40:33 -0300

    fix home

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,8 @@
 const Home = () => {
     return(
         <div>
-            <h1>Home</h1>
+            <h1>Sitio en mantenimiento ⚠️ </h1>
+            <img src="https://thumbs.gfycat.com/CookedFilthyHyracotherium-size_restricted.gif" alt="Guardian"></img>
         </div>
     )
 }
